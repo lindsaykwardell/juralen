509998d550264ea8b57942122ecf1852fbdeecec
Lindsay Wardell <three060@gmail.com>
2018-11-03 12:56:50 -0700

    Updated method of using env variables.

--- src/config/db/firebase.js
+++ src/config/db/firebase.js
@@ -1,18 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-let config = {};
-if (process.env.NODE_ENV !== "development") {
-  config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MSGR_SENDER_ID
-  };
-} else {
-  config = require("./firebase.dev");
-}
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSGR_SENDER_ID
+};
 
 firebase.initializeApp(config);
 const settings = { timestampsInSnapshots: true };
