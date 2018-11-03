5f42dca71edd6f36e6f75dba74e8083e13410622
Lindsay Wardell <three060@gmail.com>
2018-11-03 07:07:03 -0700

    Adjusting env variables to work with CRA

--- src/config/db/firebase.js
+++ src/config/db/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 let config = {};
 if (process.env.NODE_ENV !== "development") {
   config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MSGR_SENDER_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSGR_SENDER_ID
   };
 } else {
   config = require("./firebase.dev");
